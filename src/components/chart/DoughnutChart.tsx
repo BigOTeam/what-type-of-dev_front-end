910e43af937dfda501262dfc9301ac4108919b59
anottrx <anottrx@gmail.com>
2022-03-17 13:53:41 +0900

    Feat : 도넛형 차트 컴포넌트 구현

---
+++ src/components/chart/DoughnutChart.tsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react';
+
+import {
+  Chart as ChartJS,
+  ArcElement,
+  CategoryScale,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+import { ChartAnswerType, DoughnutChartData } from '../../types/chartTypes';
+import { ChartColor, CHART_COLOR_LIST } from '../../data/chartColorListData';
+
+interface ChartResultProps {
+  id: number;
+  //   question: string;
+  //   chartType: string;
+  chartAnswerInfo: ChartAnswerType[];
+}
+
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  Tooltip,
+  Legend,
+  ChartDataLabels,
+);
+
+const DoughnutChart: React.FC<ChartResultProps> = ({ id, chartAnswerInfo }) => {
+  const [data, setData] = useState<DoughnutChartData>({
+    labels: chartAnswerInfo
+      .sort((a: ChartAnswerType, b: ChartAnswerType) =>
+        a.rank > b.rank ? 1 : b.rank > a.rank ? -1 : 0,
+      )
+      .map((data: ChartAnswerType) => data.answer),
+    datasets: [
+      {
+        label: '# of Votes',
+        data: chartAnswerInfo
+          .sort((a: ChartAnswerType, b: ChartAnswerType) =>
+            a.rank > b.rank ? 1 : b.rank > a.rank ? -1 : 0,
+          )
+          .map((data: ChartAnswerType) =>
+            Math.floor(Number(data.answerCount / 90) * 100),
+          ),
+        backgroundColor: CHART_COLOR_LIST.map((x) => x.backgroundColor),
+        borderColor: CHART_COLOR_LIST.map((x) => x.borderColor),
+        borderWidth: 0,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    setData({
+      labels: chartAnswerInfo
+        .sort((a: ChartAnswerType, b: ChartAnswerType) =>
+          a.rank > b.rank ? 1 : b.rank > a.rank ? -1 : 0,
+        )
+        .map((data: ChartAnswerType) => data.answer),
+      datasets: [
+        {
+          label: '# of Votes',
+          data: chartAnswerInfo
+            .sort((a: ChartAnswerType, b: ChartAnswerType) =>
+              a.rank > b.rank ? 1 : b.rank > a.rank ? -1 : 0,
+            )
+            .map((data: ChartAnswerType) => {
+              return Math.floor(Number(data.answerCount / 90) * 100);
+            }),
+          backgroundColor: CHART_COLOR_LIST.map(
+            (chartColor: ChartColor) => chartColor.backgroundColor,
+          ),
+          borderColor: CHART_COLOR_LIST.map(
+            (chartColor: ChartColor) => chartColor.borderColor,
+          ),
+          borderWidth: 0,
+        },
+      ],
+    });
+  }, [chartAnswerInfo]);
+
+  return (
+    <div>
+      <Doughnut
+        data={data}
+        options={{
+          plugins: {
+            datalabels: {
+              formatter: function (value, context) {
+                return (
+                  value +
+                  '%\n' +
+                  context.chart.data.labels?.slice(
+                    context.dataIndex,
+                    context.dataIndex + 1,
+                  )
+                );
+              },
+              labels: {
+                value: {
+                  font: {
+                    weight: 'bold',
+                  },
+                },
+              },
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default DoughnutChart;
