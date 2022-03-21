42f60d44ccfc412ca5b50579b177426307ee1c07
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-21 17:01:25 +0900

    Feat : document 타이틀을 설정할 수 있는 커스텀 훅 구현 및 적용

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
+import useDocumentTitle from '../hooks/useDocumentTitle';
+
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 
 const HomePage: React.FC = () => {
+  useDocumentTitle('당신이 개발자라면');
+
   return (
     <>
       <Header />
