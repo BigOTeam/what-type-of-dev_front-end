d7add40730c2b15f181f09cf31966422a5d133b4
Sojjeong <tip9004@gmail.com>
2022-03-21 16:43:08 +0900

    Feat : 기본 설문 페이지 초기 구현

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import history from './history';
 
 import ErrorPage from './pages/ErrorPage';
 import HomePage from './pages/HomePage';
+import FormPage from './pages/FormPage';
 
 const App: React.FC = () => {
   return (
@@ -23,6 +24,7 @@ const App: React.FC = () => {
       <Router history={history}>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/dev-form" component={FormPage} />
         </Switch>
         {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
       </Router>
