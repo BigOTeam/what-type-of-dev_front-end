97d05270abc63defcdf37b418eba9cfb9453c57e
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-03-25 16:59:27 +0900

    Feat : 테스트 시작 섹션 컴포넌트 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import styled from '@emotion/styled';
+
 import useDocumentTitle from '../hooks/useDocumentTitle';
 
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
+import SurveyStartSection from '../components/home/SurveyStartSection';
 
 const HomePage: React.FC = () => {
   useDocumentTitle('당신이 개발자라면');
@@ -9,9 +12,17 @@ const HomePage: React.FC = () => {
   return (
     <>
       <Header />
+      <Container>
+        <SurveyStartSection />
+      </Container>
       <Footer />
     </>
   );
 };
+
+const Container = styled.main`
+  padding: 0 16px;
+  background-color: #e8f4fd;
+`;
 
 export default HomePage;
