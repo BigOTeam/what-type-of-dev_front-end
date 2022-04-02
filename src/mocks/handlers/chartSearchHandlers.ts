c75316e68cc779bf99064234beb8d1b6c6ec0044
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-04-02 20:41:13 +0900

    Test : 차트 관련 목업 API에 에러 케이스 추가

--- src/mocks/handlers/chartSearchHandlers.ts
+++ src/mocks/handlers/chartSearchHandlers.ts
@@ -14,9 +14,27 @@ export const chartSearchHandlers = [
       const chartInfoData: ChartInfoType = ChartSearchData.filter(
         (chart) => chart.gender === gender,
       ).filter((chart) => chart.age === age)[0]?.chartInfoData;
+      const status: number = 401;
+
+      if (status === 401) {
+        return response(
+          context.status(401),
+          context.json({
+            message: '잘못된 데이터 요청을 하였습니다. 다시 요청 부탁드립니다.',
+          }),
+        );
+      }
+
+      if (status === 500) {
+        return response(
+          context.status(500),
+          context.json({
+            message: '서버에 오류가 생겼습니다.',
+          }),
+        );
+      }
 
       return response(
-        context.status(200),
         context.json({
           chartInfoData,
         }),
