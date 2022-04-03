14a7d1428ee4755907b67000afbc8414f56d3b4f
Sojjeong <tip9004@gmail.com>
2022-04-03 15:18:15 +0900

    Feat : SurveyService, API 호출 함수 구현

--- src/services/SurveyService.ts
+++ src/services/SurveyService.ts
@@ -1,3 +1,38 @@
-class SurveyService {}
+import {
+  SurveyRequestType,
+  SurveyResultRequestType,
+} from '../types/SurveyType';
+import { axiosInstance } from '../utils/axios';
+
+class SurveyService {
+  public static async getSurvey(params: SurveyRequestType) {
+    const response = await axiosInstance.get('/api/v1/surveys', {
+      params,
+    });
+
+    return response.data;
+  }
+
+  public static async sendSurvey(params: SurveyResultRequestType) {
+    const response = await axiosInstance.post(
+      '/api/v1/surveys/results',
+      params,
+    );
+
+    return response.data;
+  }
+
+  public static async getJobs() {
+    const response = await axiosInstance.post('/api/v1/surveys/jobs');
+
+    return response.data;
+  }
+
+  public static async getJobDetail() {
+    const response = await axiosInstance.post('/api/v1/surveys/jobs/:jobId');
+
+    return response.data;
+  }
+}
 
 export default SurveyService;
