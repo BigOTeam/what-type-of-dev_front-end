143b4a21dcefec57ce7beac0e5b6c150154910d7
ryuna <anottrx@gmail.com>
2022-04-04 23:01:50 +0900

    Test : 유형별 상세보기 목업 API 구현

---
+++ src/mocks/handlers/jobTypeDetailHandlers.ts
@@ -0,0 +1,35 @@
+import { rest } from 'msw';
+
+import { JobTypeDetailData } from '../database/jobTypeDetail';
+
+export const jobTypeDetailHandlers = [
+  rest.get(
+    `${process.env.REACT_APP_SERVER_URL}/api/v1/surveys/jobs/:jobId`,
+    async (request, response, context) => {
+      const { jobId } = request.params;
+
+      const jobIndex = JobTypeDetailData.findIndex(
+        (job) => job.jobId === Number(jobId),
+      );
+      const status: number = 200;
+
+      if (status === 500) {
+        return response(
+          context.status(500),
+          context.json({
+            message: '서버에 오류가 생겼습니다.',
+          }),
+        );
+      }
+
+      return response(
+        context.json({
+          jobId: JobTypeDetailData[jobIndex].jobId,
+          jobName: JobTypeDetailData[jobIndex].jobName,
+          jobImgUrl: JobTypeDetailData[jobIndex].jobImgUrl,
+          jobDescription: JobTypeDetailData[jobIndex].jobDescription,
+        }),
+      );
+    },
+  ),
+];
