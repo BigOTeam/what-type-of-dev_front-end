b4eb2431fae24ad0bc5b03fa5f0afaaa874690fa
ryuna <anottrx@gmail.com>
2022-04-06 15:17:52 +0900

    Modify : 통계 차트 조건별 조회 API 수정

--- src/data/chartSearchData.ts
+++ src/data/chartSearchData.ts
@@ -5,8 +5,8 @@ interface Gender {
 }
 
 export const GENDER_LIST: Gender[] = [
-  { id: 1, name: '남성', value: 'male' },
-  { id: 2, name: '여성', value: 'female' },
+  { id: 1, name: '남성', value: '남성' },
+  { id: 2, name: '여성', value: '여성' },
 ];
 
 interface Age {
@@ -16,12 +16,12 @@ interface Age {
 }
 
 export const AGE_LIST: Age[] = [
-  { id: 1, name: '20세 미만', value: '0-20' },
-  { id: 2, name: '20 - 24세', value: '20-24' },
-  { id: 3, name: '25 - 29세', value: '25-29' },
-  { id: 4, name: '30 - 34세', value: '30-34' },
-  { id: 5, name: '35 - 39세', value: '35-39' },
-  { id: 6, name: '40 - 44세', value: '40-44' },
-  { id: 7, name: '45 - 49세', value: '45-49' },
-  { id: 8, name: '50세 이상', value: '50-100' },
+  { id: 1, name: '20세 미만', value: '20세 미만' },
+  { id: 2, name: '20 - 24세', value: '20-24세' },
+  { id: 3, name: '25 - 29세', value: '25-29세' },
+  { id: 4, name: '30 - 34세', value: '30-34세' },
+  { id: 5, name: '35 - 39세', value: '35-39세' },
+  { id: 6, name: '40 - 44세', value: '40-44세' },
+  { id: 7, name: '45 - 49세', value: '45-49세' },
+  { id: 8, name: '50세 이상', value: '50세이상' },
 ];
